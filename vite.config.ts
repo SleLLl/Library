91a7af6fa9f5edfaca20dcccdfd3b6c009644bc4
Sergei Kolyago <Slellq@gmail.com>
2021-05-03 12:24:24 +0300

    pre-release 0.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
-import compress from 'vite-plugin-compress';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [compress(), reactRefresh()],
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      webp: {
+        quality: 75,
+      },
+      mozjpeg: {
+        quality: 65,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+          {
+            removeEmptyAttrs: false,
+          },
+        ],
+      },
+    }),
+    reactRefresh(),
+  ],
 });
